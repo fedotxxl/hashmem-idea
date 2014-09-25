4ea860ce4360bdd2c60ae1e4e14498eb08c5602a
fedor.belov <fedor.belov@mail.ru>
2014-09-25 21:32:58 +0400

    v1

---
+++ src/com/hashmem/jetbrains/HashMemDialog.java
@@ -0,0 +1,14 @@
+package com.hashmem.jetbrains;
+
+import com.intellij.ide.actions.GotoFileAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/*
+ * HashMemDialog
+ * Copyright (c) 2012 Cybervision. All rights reserved.
+ */
+public class HashMemDialog extends GotoFileAction {
+    public void actionPerformed(AnActionEvent e) {
+        super.actionPerformed(e);
+    }
+}
