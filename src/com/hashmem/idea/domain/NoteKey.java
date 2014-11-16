c0a51503ff03dc1e202e14c2f61686cb9f13ecb4
fedor.belov <fedor.belov@mail.ru>
2014-11-17 00:22:29 +0400

    russian keywords support

--- src/com/hashmem/idea/domain/NoteKey.java
+++ src/com/hashmem/idea/domain/NoteKey.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public class NoteKey {
 
-    private static final Pattern VALIDATE_KEY = Pattern.compile("^[0-9a-zA-Z][0-9a-zA-Z\\.\\-_]*$");
+    private static final Pattern VALIDATE_KEY = Pattern.compile("^[0-9а-яА-Яa-zA-Z][0-9а-яА-Яa-zA-Z\\.\\-_]*$");
 
     public static boolean isValid(String key) {
         return !StringUtils.isEmpty(key) && VALIDATE_KEY.matcher(key).matches();
